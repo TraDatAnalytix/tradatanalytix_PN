74d78fbdf42ceb8f68d935f4b12cea6bc6165407
pitanjal <109843171+tradatanalytix-web@users.noreply.github.com>
2024-07-21 00:25:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -133,7 +133,7 @@ def portfolio_analytics():
         #st.write(df_nf500_list)
         #list_test = ['ADAGAS' ,'HDFCBANK', 'ICICI']
         with st.container(height=100):
-            stock_select = lc.multiselect("Select Stocks", df_symbol_list , ['IDFC', 'SBIN'])
+            stock_select = lc.container(height = 50).multiselect("Select Stocks", df_symbol_list , ['IDFC', 'SBIN'])
             uploaded_file = rc.file_uploader("Upload your portfolio holdings CSV file", type=["csv"])
         st.write("You selected:", stock_select)
 
