71d2b60560f98d7a5d714224958b219c7cbd46a0
pitanjal <109843171+tradatanalytix-web@users.noreply.github.com>
2024-07-21 19:50:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -165,13 +165,13 @@ def portfolio_analytics():
         sns.heatmap(correlation_matrix, xticklabels=correlation_matrix.columns, yticklabels=correlation_matrix.columns,
         cmap='YlGnBu', annot=True, linewidth=0.5)
         print('Correlation between Stocks in your portfolio')
-        st.pyplot(fig1)
+        lc.pyplot(fig1)
 
         daily_simple_return = df_final.pct_change(1)
         daily_simple_return.dropna(inplace=True)
         fig2, ax2 = plt.subplots(figsize = (20,10))
         daily_simple_return.plot(kind = "box",ax = ax2, title = "Risk Box Plot")
-        st.pyplot(fig2)
+        rc.pyplot(fig2)
 
 
         # extend pandas functionality with metrics, etc.
